2c39276ff0e8876a0c53e5b830a567ce1e373904
CharlesLZY <charlesliangzhiyuan@gmail.com>
2022-01-03 05:13:28 -0500

    re-upload

---
+++ corner_case.py
@@ -0,0 +1,25 @@
+### Special Case for Mainboard lower hold
+lower_hold_special_case = [
+        
+    "605056.SH", ### 咸亨国际
+    
+]
+
+
+### Special case for lottery
+lottery_date_special_case = {
+        
+    "000000.SH" : "TEST", ### FOR TEST
+    
+}
+
+### Special case for entry
+entry_special_case = {
+    
+
+    "301033.SZ" : "迈普医学(部分入围)",
+    "688779.SH":"长远锂科（未参与）",###20210730申购
+     
+    
+
+}
